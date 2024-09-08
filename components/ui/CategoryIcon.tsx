e4a3efdbb2bbecf96e31ae5bdd1ffb6a732e2eeb
Niko7even <pozzernico@gmail.com>
2024-09-08 20:50:42 -0300

    Fix: fix hacer cliqueable la categoria

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,38 +1,34 @@
-"use client"
+"use client";
 
-import Image from "next/image" // Para usar el metodo de mostrar imagenes
-//optimizado de imagenes de next
-import { Category } from "@prisma/client"
-//optimizado de router de next
-import Link from "next/link"
-// para conseguir params en componentes como este que no se puede
-import { useParams } from "next/navigation"
+import Image from "next/image"; // Para usar el metodo de mostrar imagenes optimizado de next
+import { Category } from "@prisma/client"; // Optimizado de router de next
+import Link from "next/link"; // Para manejar enlaces
+import { useParams } from "next/navigation"; // Para conseguir params en componentes como este que no se puede
 
 type CategoryIconProps = {
-    category: Category
-}
+    category: Category;
+};
 
-export default function CategoryIcon({category}: CategoryIconProps) {
-    const params = useParams<{category: string}>();
+export default function CategoryIcon({ category }: CategoryIconProps) {
+    const params = useParams<{ category: string }>();
 
-  return (
-    <div
-        className={`${category.slug === params.category ? 
-            'bg-neutral-200' : ''} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
-    >
-        <div className="w-16 h-16 relative">
-            <Image 
-                fill
-                src={`/icon_${category.slug}.svg`} 
-                alt="Imagen Categoria"
-            />
-        </div>
-
+    return (
         <Link
-            className="text-xl font-bold cursor-pointer"
             href={`/order/${category.slug}`}
+            className={`${category.slug === params.category ? 'bg-neutral-200' : ''
+            } flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
         >
-        {category.name}</Link>
-    </div>
-  )
+            <div className="w-16 h-16 relative flex-shrink-0">
+                <Image
+                    fill
+                    src={`/icon_${category.slug}.svg`}
+                    alt={`Icono de la categoría ${category.name}`}
+                />
+            </div>
+
+            <span className="text-xl font-bold">
+        {category.name}
+      </span>
+        </Link>
+    );
 }
